66a5c89a6b072b7f7f19be4a6b14e0c759489e90
Nicolas Rosendo <nicolasrosendo@hotmail.com>
2019-05-30 18:29:46 -0300

    change appId

--- app.js
+++ app.js
@@ -1,40 +1,43 @@
-const OctadeskIframe = require("@octadesk/iframe");
+const OctadeskIframe = require('@octadesk/iframe');
 
 // create a instance of service with your app id
-const octadeskIframe = new OctadeskIframe(
-  "86f854c4-4582-e911-bc42-526af7764f64"
-);
+window.addEventListener('load', function() {
+  const octadeskIframe = new OctadeskIframe(
+    'c454fade-8245-11e9-bc42-526af7764f64'
+  );
 
-// listen the octadesk events
-window.addEventListener("octadesk_onSendUserLogged", function(e) {
-  const user = e.detail;
-  console.log("octadesk_onSendUserLogged: ", user);
+  // listen the octadesk events
+  window.addEventListener('octadesk_onSendUserLogged', function(e) {
+    const user = e.detail;
+    console.log('octadesk_onSendUserLogged: ', user);
+  });
+
+  window.addEventListener('octadesk_onSendUserToken', function(e) {
+    const userToken = e.detail;
+    console.log('octadesk_onSendUserToken: ', userToken);
+  });
+
+  window.addEventListener('octadesk_onOpenTicket', function(e) {
+    const ticket = e.detail;
+    console.log('octadesk_onOpenTicket: ', ticket);
+  });
+
+  window.addEventListener('octadesk_onOpenPerson', function(e) {
+    const person = e.detail;
+    console.log('octadesk_onOpenPerson: ', person);
+  });
+
+  window.addEventListener('octadesk_onAfterSaveTicket', function(e) {
+    const ticket = e.detail;
+    console.log('octadesk_onAfterSaveTicket: ', ticket);
+  });
+
+  window.addEventListener('octadesk_onAfterSavePerson', function(e) {
+    const person = e.detail;
+    console.log('octadesk_onAfterSavePerson: ', person);
+  });
+
+  // tell octadesk to connect
+  octadeskIframe.connect();
+  console.log('App connected');
 });
-
-window.addEventListener("octadesk_onSendUserToken", function(e) {
-  const userToken = e.detail;
-  console.log("octadesk_onSendUserToken: ", userToken);
-});
-
-window.addEventListener("octadesk_onOpenTicket", function(e) {
-  const ticket = e.detail;
-  console.log("octadesk_onOpenTicket: ", ticket);
-});
-
-window.addEventListener("octadesk_onOpenPerson", function(e) {
-  const person = e.detail;
-  console.log("octadesk_onOpenPerson: ", person);
-});
-
-window.addEventListener("octadesk_onAfterSaveTicket", function(e) {
-  const ticket = e.detail;
-  console.log("octadesk_onAfterSaveTicket: ", ticket);
-});
-
-window.addEventListener("octadesk_onAfterSavePerson", function(e) {
-  const person = e.detail;
-  console.log("octadesk_onAfterSavePerson: ", person);
-});
-
-// tell octadesk to connect
-octadeskIframe.connect();
